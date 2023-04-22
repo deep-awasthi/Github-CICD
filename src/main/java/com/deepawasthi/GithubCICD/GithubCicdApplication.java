5985309ce25280611c71863c14df1f534f949e82
Deep Awasthi <da.madskull@gmail.com>
2023-04-23 04:35:42 +0530

    Update GithubCicdApplication.java

--- src/main/java/com/deepawasthi/GithubCICD/GithubCicdApplication.java
+++ src/main/java/com/deepawasthi/GithubCICD/GithubCicdApplication.java
@@ -2,12 +2,19 @@ package com.deepawasthi.GithubCICD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdApplication.class, args);
 	}
 
+	@GetMapping("/home")
+	public String home(){
+		return "WELCOME";
+	}
 }
